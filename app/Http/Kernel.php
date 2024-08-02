d7ace39428c97ba11aec9ba346d66484cda79973
Itok toni laksono <itok.toni@gmail.com>
2024-08-02 20:38:56 +0700

    update cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Http\Middleware\HandleCors::class
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class
     ];
 
     /**
