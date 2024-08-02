956819504e89b4ea2dbb3c07dd200dbfa5d0d4e6
Itok toni laksono <itok.toni@gmail.com>
2024-08-02 19:50:48 +0700

    update

--- app/Http/Controllers/Core/HomeController.php
+++ app/Http/Controllers/Core/HomeController.php
@@ -27,7 +27,6 @@ class HomeController extends Controller
      */
     public function index(Dashboard $chart)
     {
-        ds(true);
         if(auth()->check() && auth()->user()->active == false){
             return redirect()->route('login');
         }
