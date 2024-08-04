3cc88c8029861103f4b9062eb700a7d6c0fbf5b9
itoktoni <itok.toni@gmail.com>
2024-08-04 22:34:07 +0700

    update auto deployment

---
+++ app/Http/Controllers/Core/WebhookController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Core;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class WebhookController extends Controller
+{
+    public function deploy(Request $request)
+    {
+        $githubPayload = $request->getContent();
+        $githubHash = $request->header('X-Hub-Signature');
+        $localToken = env('GITHUB_WEBHOOK_SECRET');
+        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+        if (hash_equals($githubHash, $localHash)) {
+
+            Artisan::call('app:deploy');
+
+        }
+    }
+}
