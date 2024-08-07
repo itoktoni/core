fdb6453aafb6928be9aee7fd077583dc5047eb2e
itoktoni <itok.toni@gmail.com>
2024-08-07 23:37:50 +0700

    update symphony proses

--- app/Http/Controllers/Core/WebhookController.php
+++ app/Http/Controllers/Core/WebhookController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Core;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class WebhookController extends Controller
 {
@@ -16,8 +17,16 @@ class WebhookController extends Controller
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
         if (hash_equals($githubHash, $localHash)) {
 
-            Artisan::call('app:deploy');
+            chdir(base_path());
+            $process = new Process(['git', 'pull']);
+            $process->run();
 
+            // executes after the command finishes
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            return $process->getOutput();
         }
     }
 }
